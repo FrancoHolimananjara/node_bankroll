bb9141ee3589b98d5f9cfcd5c8a9a1314fb6239e
Franco <andriamitantsoa36kofra@gmail.com>
2024-02-14 14:07:23 +0300

    edit token expiration

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,20 +1,20 @@
-const jwt = require('jsonwebtoken');
-const { secretKey } = require('../config/secretKey');
+const jwt = require("jsonwebtoken");
+const { secretKey } = require("../config/secretKey");
 
-const generateToken = ({_id,username}) => {
-    return jwt.sign(
+const generateToken = ({ _id, username }) => {
+  return jwt.sign(
     // payload
-        {
-            _userId : _id,
-            username
-        },
+    {
+      _userId: _id,
+      username,
+    },
     // key
-        secretKey,
+    secretKey,
     // delay
-        {
-            expiresIn: '5min'
-        }
-    )
-}
+    {
+      expiresIn: "2h",
+    }
+  );
+};
 
 module.exports = generateToken;
